e10c1002f46c7b83101e37aa43d4fb61408e4912
Dhanya P <pdhanya098@gmail.com>
2026-09-07 17:10:31 +0530

    Day 10:react state, props, and a working to-do list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+function Greeting(props) {
+  return <h2>Hello, {props.name}!</h2>;
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I'm Dhanya. This is my first React app!
         </p>
         <a
           className="App-link"
@@ -18,8 +21,36 @@ function App() {
           Learn React
         </a>
       </header>
+      <Greeting name="Dhanya" />
+<Greeting name="Anu" />
+<TodoList />
     </div>
   );
 }
+function TodoList() {
+  const [todos, setTodos] = useState(["Learn JS", "Learn React"]);
+  const [newTodo, setNewTodo] = useState("");
 
+  function addTodo() {
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  }
+
+  return (
+    <div>
+      <h2>My To-Do List</h2>
+      <input
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add a task"
+      />
+      <button onClick={addTodo}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 export default App;
